2c848ca1beb2c558c9af5c2684799c46b0f03313
Jonas Spieler <mail@spieler.io>
2020-03-28 10:49:24 +0100

    Change button when resize to mobile

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
+// include {scaleLinear} from "node_modules/d3-scale";
+
 window.onload = () => {
+  const IINITIAL_WINDOW_SIZE = document.documentElement.clientWidth;
+  const MOBILE_WINDOW_SIZE = 768;
   const infobar = document.querySelector('#infobar');
   const infobarButton = document.querySelector('#infobarButton');
+
+  if (MOBILE_WINDOW_SIZE < IINITIAL_WINDOW_SIZE) {
+    infobar.setAttribute('data-mobile', 'false');
+  }
 
   const menuControl = () => {
     infobar.classList.toggle('toggled');
@@ -8,17 +16,22 @@ window.onload = () => {
 
   const changeButton = () => {
     const w = document.documentElement.clientWidth;
-    if (w < 768) {
-      const btn = document.createElement('i');
-      btn.setAttribute('class', 'fas fa-ellipsis-v');
-      infobarButton.firstChild = btn;
-    } else {
+    console.log(infobar.dataset['mobile']);
+    if (infobar.dataset['mobile'] === 'false' && w < MOBILE_WINDOW_SIZE) {
       const btn = document.createElement('i');
       btn.setAttribute('class', 'fas fa-ellipsis-h');
-      infobarButton.firstChild = btn;
+      infobarButton.innerHTML = '';
+      infobarButton.appendChild(btn);
+      infobar.dataset['mobile'] = 'true';
+    } else if (infobar.dataset['mobile'] === 'true' && w >= MOBILE_WINDOW_SIZE) {
+      const btn = document.createElement('i');
+      btn.setAttribute('class', 'fas fa-ellipsis-v');
+      infobarButton.innerHTML = '';
+      infobarButton.appendChild(btn);
+      infobar.dataset['mobile'] = 'false';
     }
   }
 
   infobarButton.addEventListener('click', menuControl);
-  infobarButton.addEventListener('resize', changeButton);
+  window.addEventListener('resize', changeButton);
 };
