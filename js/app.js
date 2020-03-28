2f15bc7046f800122ece15c07dd0215c94ea5702
PatrickZuest <32989083+PatrickZuest@users.noreply.github.com>
2020-03-28 23:30:25 +0100

    free the functions

--- js/app.js
+++ js/app.js
@@ -2,10 +2,43 @@ let currentDay = 40;
 let playInterval;
 let lastDay;
 
+const playButton = document.querySelector('#playButton');
+const nextDay = document.querySelector('#nextButton');
+const prevDay = document.querySelector('#prevButton');
+const infobar = document.querySelector('#infobar');
+const infobarButton = document.querySelector('#infobarButton');
+
+const changeButton = (type) => {
+  playButton.innerHTML = "";
+  const icon = document.createElement('i');
+  if (type === 'play') {
+    icon.setAttribute('class', 'fas fa-pause-circle');
+  } else if (type === 'pause'){
+    icon.setAttribute('class', 'fas fa-play-circle');
+  }
+  console.log(icon);
+  playButton.appendChild(icon);
+};
+
+const animationControl = () => {
+  const mode = playButton.dataset['mode']
+  if (mode === 'play') {
+    stopAnimation();
+  } else {
+    playAnimation();
+  }
+}
+
 function playAnimation(){
+  changeButton('play');
+  playButton.dataset['mode'] = 'play';
   playInterval = setInterval(
     function() {
-      increaseDay(1);
+      if(currentDay == lastDay) {
+        stopAnimation();
+      } else {
+        increaseDay(1);
+      }
     }, 100);
 }
 
@@ -25,39 +58,11 @@ function decreaseDay(step){
 
 function stopAnimation() {
   clearInterval(playInterval);
+  changeButton('pause');
+  playButton.dataset['mode'] = 'pause';
 }
 
 window.onload = () => {
-  const playButton = document.querySelector('#playButton');
-  const nextDay = document.querySelector('#nextButton');
-  const prevDay = document.querySelector('#prevButton');
-  const infobar = document.querySelector('#infobar');
-  const infobarButton = document.querySelector('#infobarButton');
-
-  const changeButton = (type) => {
-    playButton.innerHTML = "";
-    const icon = document.createElement('i');
-    if (type === 'play') {
-      icon.setAttribute('class', 'fas fa-pause-circle');
-    } else if (type === 'pause'){
-      icon.setAttribute('class', 'fas fa-play-circle');
-    }
-    console.log(icon);
-    playButton.appendChild(icon);
-  };
-  
-  const animationControl = () => {
-    const mode = playButton.dataset['mode']
-    if (mode === 'play') {
-      stopAnimation();
-      changeButton('pause');
-      playButton.dataset['mode'] = 'pause';
-    } else {
-      playAnimation();
-      changeButton('play');
-      playButton.dataset['mode'] = 'play';
-    }
-  }
 
   const menuControl = () => {
     infobar.classList.toggle('toggled');
