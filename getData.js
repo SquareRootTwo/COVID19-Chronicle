2e27d181ec9f73b23d394c7722439be60a2d8945
PatrickZuest <32989083+PatrickZuest@users.noreply.github.com>
2020-03-28 15:54:17 +0100

    get data from remote file

--- getData.js
+++ getData.js
@@ -1,6 +1,6 @@
 var mapData = d3.map();
 
-d3.csv("corona2.csv", function(error, data) {
+d3.csv("https://raw.githubusercontent.com/SquareRootTwo/COVID19-Chronicle/master/corona2.csv", function(error, data) {
     data.forEach(function(entry) {
         mapData.set(entry.id, entry.infected);
     });
