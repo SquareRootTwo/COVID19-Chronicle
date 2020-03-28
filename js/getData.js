0ca447bb9f9e9cb13edd7fd2cccf600e2f6cd85b
PatrickZuest <32989083+PatrickZuest@users.noreply.github.com>
2020-03-28 20:20:36 +0100

    change US

--- js/getData.js
+++ js/getData.js
@@ -1,8 +1,9 @@
 var mapData = d3.map();
 
-d3.csv("https://raw.githubusercontent.com/SquareRootTwo/COVID19-Chronicle/master/corona2.csv", function(error, data) {
+d3.csv("https://raw.githubusercontent.com/SquareRootTwo/COVID19-Chronicle/master/Covid_Data.csv", function(error, data) {
+    console.log(data);
     data.forEach(function(entry) {
-        mapData.set(entry.id, entry.infected);
+        mapData.set(entry.id, entry['66_i']);
     });
     initMap();
 });
